00f4c00425bf9ee09352b385ec1ceac40484d7ec
Ablazzing <y2286122@yandex.ru>
2021-06-03 16:52:00 +0300

    create template for rest

---
+++ src/main/java/config/MyConfig.java
@@ -0,0 +1,58 @@
+package config;
+
+
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+import org.hibernate.SessionFactory;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.orm.hibernate5.HibernateTransactionManager;
+import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
+import org.springframework.transaction.annotation.EnableTransactionManagement;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+import javax.sql.DataSource;
+import java.beans.PropertyVetoException;
+import java.util.Properties;
+
+@Configuration
+@ComponentScan(basePackages = "com.molodyko.spring_rest")
+@EnableWebMvc
+@EnableTransactionManagement
+public class MyConfig {
+
+    @Bean
+    public DataSource dataSource(){
+        ComboPooledDataSource comboPooledDataSource = new ComboPooledDataSource();
+        try {
+            comboPooledDataSource.setDriverClass("com.mysql.cj.jdbc.Driver");
+            comboPooledDataSource.setJdbcUrl("jdbc:mysql://localhost:3306/my_db?useSSL=false&amp;serverTimezone=UTC");
+            comboPooledDataSource.setUser("bestuser");
+            comboPooledDataSource.setPassword("bestuser");
+        } catch (PropertyVetoException e) {
+            e.printStackTrace();
+        }
+        return comboPooledDataSource;
+    }
+
+    @Bean
+    public LocalSessionFactoryBean localSessionFactoryBean(){
+        LocalSessionFactoryBean sessionFactoryBean = new LocalSessionFactoryBean();
+        sessionFactoryBean.setDataSource(dataSource());
+        sessionFactoryBean.setPackagesToScan("com.molodyko.mvc_plus_hibernate");
+        Properties properties = new Properties();
+        properties.setProperty("hibernate.dialect","org.hibernate.dialect.MySQLDialect");
+        properties.setProperty("hibernate.show_sql","true");
+        sessionFactoryBean.setHibernateProperties(properties);
+        return sessionFactoryBean;
+    }
+
+    @Bean
+    public HibernateTransactionManager hibernateTransactionManager(){
+        HibernateTransactionManager hibernateTransactionManager = new HibernateTransactionManager();
+        hibernateTransactionManager.setSessionFactory(localSessionFactoryBean().getObject());
+        return hibernateTransactionManager;
+    }
+
+
+}
