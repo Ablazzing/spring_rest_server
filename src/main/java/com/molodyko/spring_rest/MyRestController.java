711c16dd02cd0f4433b9af0575fdac81362b5d74
Ablazzing <y2286122@yandex.ru>
2021-06-04 17:34:34 +0300

    create Controller advice (global excetion handler)

--- src/main/java/com/molodyko/spring_rest/MyRestController.java
+++ src/main/java/com/molodyko/spring_rest/MyRestController.java
@@ -32,19 +32,5 @@ public class MyRestController {
         return employee;
     }
 
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleException(NoSuchEmployee exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
 
-        return new ResponseEntity<>(data, HttpStatus.NOT_FOUND);
-    }
-
-    @ExceptionHandler
-    public ResponseEntity<EmployeeIncorrectData> handleAnotherException(Exception exception){
-        EmployeeIncorrectData data = new EmployeeIncorrectData();
-        data.setInfo(exception.getMessage());
-
-        return new ResponseEntity<>(data, HttpStatus.BAD_REQUEST);
-    }
 }
